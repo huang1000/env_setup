724562307273a3c2180315b4ccdb4bb1aae75c9f
Huang Dong <dong.huang@shopee.com>
2018-08-23 10:07:43 +0800

    add setup_env.sh

---
+++ setup_env.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+cd 
+
+# install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install zsh
+apt-get install zsh
+# install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#git clone git@github.com:huang1000/env_setup.git 
+git clone https://github.com/huang1000/env_setup.git
